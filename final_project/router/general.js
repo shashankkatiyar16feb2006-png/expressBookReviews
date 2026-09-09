b013a5df7e37e964f50f2862e22ad1c0a26bbc50
Shashank Katiyar <shashankkatiyar16feb2006@gmail.com>
2026-09-09 12:01:57 -0400

    completed task 11 using axios async await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -58,6 +58,19 @@ public_users.get('/isbn/:isbn', function (req, res) {
     res.json(books[isbn]);
 
 });
+
+// Task 11: Get book details by ISBN using Async/Await with Axios
+public_users.get('/async/isbn/:isbn', async function (req, res) {
+    try {
+        const isbn = req.params.isbn;
+        const response = await axios.get('http://localhost:5000/isbn/' + isbn);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({
+            message: "Error fetching book by ISBN"
+        });
+    }
+});
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
 
